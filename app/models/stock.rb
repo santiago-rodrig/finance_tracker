13175d2c3a8a70144be8583da649b4bb688a5d5f
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-20 13:48:03 -0400

    feature: symbol lookup working in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,14 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    IEX::Api::Client.new(
+    client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex[:sandbox_public_key],
       secret_token: Rails.application.credentials.iex[:sandbox_secret_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
-    ).price(ticker_symbol)
+    )
+    new(
+      ticker: ticker_symbol,
+      name: client.company(ticker_symbol).company_name,
+      last_price: client.price(ticker_symbol)
+    )
   end
 end
