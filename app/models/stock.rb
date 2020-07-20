79b3459ea440997152e4f6a6fbdfcde6a3701acf
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-20 14:00:42 -0400

    feature: stock search error handling

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,10 +5,14 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex[:sandbox_secret_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    new(
-      ticker: ticker_symbol,
-      name: client.company(ticker_symbol).company_name,
-      last_price: client.price(ticker_symbol)
-    )
+    begin
+      new(
+        ticker: ticker_symbol,
+        name: client.company(ticker_symbol).company_name,
+        last_price: client.price(ticker_symbol)
+      )
+    rescue
+      nil
+    end
   end
 end
