e1b8d88b7641326e5cc9cedfe1809027ccba9669
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-20 17:34:40 -0400

    feature: stock search validation with js

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,12 +8,19 @@ class StocksController < ApplicationController
           fmt.html { render 'users/my_portfolio' }
         end
       else
-        flash[:alert] = 'Symbol not found.'
-        redirect_to my_portfolio_url
+        flash.now[:alert] = 'Symbol not found.'
+        respond_to do |fmt|
+          fmt.js { render partial: 'users/result' }
+          fmt.html { render 'users/my_portfolio' }
+        end
       end
     else
-      flash[:alert] = 'Please enter a symbol to search.'
-      redirect_to my_portfolio_url
+      flash.now[:alert] = 'Please enter a symbol to search.'
+      respond_to do |fmt|
+        fmt.js { render partial: 'users/result' }
+        fmt.html { render 'users/my_portfolio' }
+      end
     end
   end
 end
+
