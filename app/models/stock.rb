3ea195344909fe12c8cba111739687d566f90e07
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-20 12:43:07 -0400

    feature: stocks price lookup for stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex[:sandbox_public_key],
+      secret_token: Rails.application.credentials.iex[:sandbox_secret_key],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    ).price(ticker_symbol)
+  end
 end
