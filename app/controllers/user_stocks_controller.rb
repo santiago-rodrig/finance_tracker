50977bc7b888be805693d439bbaf36d7ec3b64d8
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-21 08:27:57 -0400

    feature: delete stocks from list

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -8,4 +8,16 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio."
     redirect_to my_portfolio_url
   end
+
+  def destroy
+    @user_stock = UserStock.find_by(user_id: params[:user], stock_id: params[:stock_tracked])
+    @user = User.find(params[:user])
+    @user_stock&.destroy
+    @tracked_stocks = @user.reload.stocks
+    flash.now[:notice] = 'Stock was successfully deleted.'
+    respond_to do |fmt|
+      fmt.js { render }
+      fmt.html { render 'users/my_portfolio' }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: :create
+  delete 'user_stocks/:user/:stock_tracked', to: 'user_stocks#destroy', as: 'user_stock'
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
 end
+
