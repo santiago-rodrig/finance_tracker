cd9a67e6af999b480461a3941d57c060227718f1
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-26 19:55:30 -0400

    fix: no rescue block in job file

--- app/jobs/update_companies_job.rb
+++ app/jobs/update_companies_job.rb
@@ -2,19 +2,15 @@ class UpdateCompaniesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    begin
-      all_stocks = Stock.all
-      all_stocks.each do |s|
-        s.update(Stock.new_lookup(s.ticker).to_h)
-      end
-      UpdateCompaniesJob.set(wait: 1.minute).perform_later
-      ActionCable.server.broadcast(
-        'stock_changes',
-        stocks: all_stocks
-      )
-    rescue
-      nil
+    all_stocks = Stock.all
+    all_stocks.each do |s|
+      s.update(Stock.new_lookup(s.ticker).to_h)
     end
+    UpdateCompaniesJob.set(wait: 1.minute).perform_later
+    ActionCable.server.broadcast(
+      'stock_changes',
+      stocks: all_stocks
+    )
   end
 end
 
