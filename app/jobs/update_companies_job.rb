708b4e46108e25d20a98f8d57d23818dcc34ec80
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-26 19:51:35 -0400

    feature: rescue missing stock class

--- app/jobs/update_companies_job.rb
+++ app/jobs/update_companies_job.rb
@@ -2,15 +2,19 @@ class UpdateCompaniesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    all_stocks = Stock.all
-    all_stocks.each do |s|
-      s.update(Stock.new_lookup(s.ticker).to_h)
+    begin
+      all_stocks = Stock.all
+      all_stocks.each do |s|
+        s.update(Stock.new_lookup(s.ticker).to_h)
+      end
+      UpdateCompaniesJob.set(wait: 1.minute).perform_later
+      ActionCable.server.broadcast(
+        'stock_changes',
+        stocks: all_stocks
+      )
+    rescue
+      nil
     end
-    UpdateCompaniesJob.set(wait: 1.minute).perform_later
-    ActionCable.server.broadcast(
-      'stock_changes',
-      stocks: all_stocks
-    )
   end
 end
 
