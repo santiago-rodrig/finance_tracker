b00f13d9678896641d72ff6eafe099fba8064c3d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-26 20:23:43 -0400

    setup: redis in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # actioncable
+  config.web_socket_server_url = 'wss://srodrig-finance-tracker.herokuap.com/cable'
+  config.action_cable.allowed_request_origins = ['https://srodrig-finance-tracker.herokuapp.com', 'http://srodrig-finance-tracker.herokuapp.com']
 end
